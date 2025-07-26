dcb48e181d79b1a309304a3df785eaa5ef57d59c
Paulo <pcrrp.pereira@gmail.com>
2025-07-25 21:01:42 -0300

    Exercício para prática de POO, dando continuidade no código adicionando uma função para adicionar um saldo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,52 @@
 package application;
 
+import entities.Count;
+
+import java.util.Scanner;
+
 public class Program {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+
+        // iniciando o cadastro pedindo o número da conta
+        System.out.println("Número da conta: ");
+        int numberCount = entrada.nextInt();
+        entrada.nextLine();
+
+        // pedindo o nome
+        System.out.println(); //pula uma linha
+        System.out.println("Insira seu nome: ");
+        String name = entrada.nextLine();
+
+        // pedindo para fazer um depósito inicial
+        System.out.println();
+        System.out.println("Gostaria de fazer um depósito? (s/n):");
+        String resposta = entrada.nextLine().trim();
+        double balance = 0;
+
+        if (resposta.equalsIgnoreCase("s")){
+            System.out.println("Valor do depósito inicial: R$ ");
+            balance = entrada.nextDouble();
+        }
+
+        // criando a conta
+        Count count = new Count(numberCount, name, balance);
+        System.out.println();// pula uma linha
+        System.out.println("Conta criada com sucesso!");
+        System.out.println(count);
+
+        // pedindo para adicionar valor no saldo
+        System.out.println("Adicione qualquer valor no seu saldo: ");
+        balance = entrada.nextDouble();
+        count.addBalance(balance);
+
+        System.out.println();
+        System.out.println("Dados atualizados: " + count);
+
+
+        entrada.close();
+    }
+
+
 }
--- src/entities/Count.java
+++ src/entities/Count.java
@@ -1,4 +1,43 @@
 package entities;
 
 public class Count {
+
+// número da conta
+// nome do titular da conta
+// valor de depósito inicial ao abrir a conta pela primeira vez (é opcional)
+
+    private final int numberCount;
+    private final String name;
+    private double balance;
+
+    public Count(int numberCount, String name, double balance){
+        this.numberCount = numberCount;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public int getNumberCount(){
+        return numberCount;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    // adicionando saldo na conta
+    public void addBalance(double balance){
+        this.balance += balance;
+    }
+
+// para imprimir as informações
+
+    public String toString(){
+        return "Número da conta: " + numberCount + ", Nome: " + name + ", saldo de $" + String.format("%.2f", balance);
+
+    }
+
 }
