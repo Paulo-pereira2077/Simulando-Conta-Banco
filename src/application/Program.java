5665b35ed617c9d9cc910ba27dcb32d05de7018c
Paulo <pcrrp.pereira@gmail.com>
2025-07-25 22:49:25 -0300

    Exercício para prática de POO, fazendo algumas alterações nas funções para facilitar a compreensão e futuras alterações no código.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import entities.Count;
+import entities.Account;
 
 import java.util.Scanner;
 
@@ -9,9 +9,12 @@ public class Program {
     public static void main(String[] args) {
         Scanner entrada = new Scanner (System.in);
 
+        // criando a conta
+        Account account;
+
         // iniciando o cadastro pedindo o número da conta
         System.out.print("Número da conta: ");
-        int numberCount = entrada.nextInt();
+        int numberAccount = entrada.nextInt();
         entrada.nextLine();
 
         // pedindo o nome
@@ -22,44 +25,48 @@ public class Program {
         // pedindo para fazer um depósito inicial
         System.out.println(); // pula uma linha
         System.out.print("Gostaria de fazer um depósito? (s/n):");
-        String resposta = entrada.nextLine().trim();
-        double balance = 0;
+        // trocando de String para char e removendo o 'double balance = 0;'
+        char resposta = entrada.next().charAt(0);
 
-        if (resposta.equalsIgnoreCase("s")){
+        if (resposta == 's'){
             System.out.println();// pula uma linha
             System.out.print("Valor do depósito inicial: R$ ");
-            balance = entrada.nextDouble();
+            // trocando de 'balance' para initialDeposit
+            double initialDeposit = entrada.nextDouble();
+            // criando a conta caso escolha adicionar um depósito inicial
+            account = new Account(numberAccount, name, initialDeposit);
+        }
+        else{
+            account = new Account(numberAccount, name);
         }
 
-        // criando a conta
-        Count count = new Count(numberCount, name, balance);
         System.out.println();// pula uma linha
         System.out.println("Conta criada com sucesso!");
-        System.out.println(count);
+        System.out.println(account);
 
 
         // pedindo para adicionar valor no saldo
         System.out.println(); // pula uma linha
         System.out.print("Adicione qualquer valor no seu saldo: ");
-        balance = entrada.nextDouble();
+       double balance = entrada.nextDouble();
         entrada.nextLine();
-        count.addBalance(balance);
+        account.deposit(balance);
 
         System.out.println();
-        System.out.println("Dados atualizados: " + count);
+        System.out.println("Dados atualizados: " + account);
 
         // pedindo para sacar o dinheiro
         System.out.println(); // pula uma linha
         System.out.print("Gostaria de sacar o dinheiro? (s/n): ");
-        resposta = entrada.nextLine().trim();
-        if (resposta.equalsIgnoreCase("s")) {
+        resposta = entrada.next().charAt(0);
+        if (resposta == 's') {
             System.out.print("Valor do saque: R$ ");
             balance = entrada.nextDouble();
-            count.removeBalance(balance);
+            account.removeBalance(balance);
         }
 
         System.out.println();
-        System.out.println("Dados atualizados: " + count);
+        System.out.println("Dados atualizados: " + account);
 
         entrada.close();
     }
---
+++ src/entities/Account.java
@@ -0,0 +1,59 @@
+package entities;
+
+public class Account {
+
+// número da conta
+// nome do titular da conta
+// valor de depósito inicial ao abrir a conta pela primeira vez (é opcional)
+
+    private final int numberAccount;
+    private String name;
+    private double balance;
+
+    // para caso o usuário escolha a opção de não fazer um depósito inicial
+    public Account(int numberAccount, String name) {
+        this.numberAccount = numberAccount;
+        this.name = name;
+    }
+
+    // adicionando o depósito inicial da conta, já que uma conta quando criada tem o valor de R$ 0,00
+    public Account(int numberCount, String name, double initialDeposit){
+        this.numberAccount = numberCount;
+        this.name = name;
+        deposit(initialDeposit); // para o depósito inicial
+    }
+
+    public int getNumberAccount(){
+        return numberAccount;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    // adicionando saldo na conta e trocando a nomenclatura de addBalance para deposit e criando uma 'quantia (amount)'
+    public void deposit(double amount){
+        balance += amount;
+    }
+
+    // sacando dinheiro da conta com uma taxa de R$ 5,00 e e criando uma 'quantia (amount)'
+    public void removeBalance(double amount){
+        balance -= amount + 5.0;
+    }
+
+// para imprimir as informações
+
+    public String toString(){
+        return "Número da conta: " + numberAccount + ", Nome: " + name + ", saldo de $" + String.format("%.2f", balance);
+
+    }
+
+}
